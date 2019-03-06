b7753f561a96e4e2e58c18118eeceda99cb9b038
Roman Kagan <roman@rumble.me>
2019-03-06 11:03:00 +0200

    implement get/post

--- src/main/java/com/argus/exercise/ExcersiceController.java
+++ src/main/java/com/argus/exercise/ExcersiceController.java
@@ -1,9 +1,19 @@
 package com.argus.exercise;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class ExcersiceController {
+        private String _currentValue;
+
+        @RequestMapping(value="/api/resource", method=RequestMethod.POST)
+        public void postResource(@RequestBody String payload) {
+            _currentValue = payload;
+        }
+
+        @RequestMapping(value="/api/resource", method=RequestMethod.GET)
+        public String getResource() {
+            return _currentValue;
+        }
+
 }
