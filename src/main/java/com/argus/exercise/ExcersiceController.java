c35afb215d1191cbfe67e7696cd7fdd3f73d57d6
Roman Kagan <roman@rumble.me>
2019-03-06 10:21:56 +0200

    add rest controller

---
+++ src/main/java/com/argus/exercise/ExcersiceController.java
@@ -0,0 +1,9 @@
+package com.argus.exercise;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ExcersiceController {
+}
