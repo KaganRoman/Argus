ac3f3d5df994f51ec6513233a3b48cc51f84fbff
Roman Kagan <roman@rumble.me>
2019-03-06 11:48:17 +0200

    update mirror

--- src/main/java/com/argus/exercise/ExcersiceController.java
+++ src/main/java/com/argus/exercise/ExcersiceController.java
@@ -1,14 +1,28 @@
 package com.argus.exercise;
 
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.MalformedURLException;
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class ExcersiceController {
         private String _currentValue;
 
+        @Value("${mirror}")
+        private String _mirror;
+
         @RequestMapping(value="/api/resource", method=RequestMethod.POST)
         public void postResource(@RequestBody String payload) {
-            _currentValue = payload;
+            if(_currentValue != payload) {
+                _currentValue = payload;
+                updateMirror();
+            }
         }
 
         @RequestMapping(value="/api/resource", method=RequestMethod.GET)
@@ -16,4 +30,24 @@ public class ExcersiceController {
             return _currentValue;
         }
 
+        private void updateMirror() {
+            if(_mirror == null) return;
+            try
+            {
+                URL url = new URL("http://" + _mirror + "/api/resource");
+                HttpURLConnection httpCon = (HttpURLConnection) url.openConnection();
+                httpCon.setDoOutput(true);
+                httpCon.setRequestMethod("POST");
+                OutputStream os = httpCon.getOutputStream();
+                OutputStreamWriter osw = new OutputStreamWriter(os, "UTF-8");
+                osw.write(_currentValue);
+                osw.flush();
+                osw.close();
+                os.close();
+                httpCon.connect();
+            }catch(MalformedURLException e){
+            }catch(IOException e){
+            }catch(Exception e){
+            }
+        }
 }
